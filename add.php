c8fc6386631dacd2be34a12fca8e37668cda493c
boros123 <nauvaall.qa@gmail.com>
2023-12-24 22:04:17 +0700

    y-1

--- add.php
+++ add.php
@@ -1,56 +1,71 @@
 <?php
-    include 'config.php';
+include 'config.php';
 
-    if (isset($_POST["submit"])) {
-        $nama = $_POST['nama'];
-        $nik = $_POST['nik'];
-        $jk = $_POST['jk'];
-        $kecamatan = $_POST['kecamatan'];
-        $kota = $_POST['kota'];
-        $provinsi = $_POST['provinsi'];
+if (isset($_POST["submit"])) {
 
-        $query = "INSERT INTO warga (nama, nik, jk, kecamatan, kota, provinsi) VALUES ('$nama', '$nik', '$jk', '$kecamatan', '$kota', '$provinsi')";
-        $koneksi->query($query);
-
-        header("Location: data.php");
-        return 0;
+    if (isset($_POST["simpan"])) {
+        if ($_POST["nama"] == "" | $_POST["nik"] == "" | $_POST["jk"] == "" | $_POST["kecamatan"] == "" | $_POST["kota"] == "" | $_POST["provinsi"] == "") {
+            // cek
+            echo '<script>
+             alert("Jangan ada yang kosong !");
+             windows.location="kelompok-main/edit.php";
+            </script>';
+            return false;
+        }
     }
+
+
+    $nama = $_POST['nama'];
+    $nik = $_POST['nik'];
+    $jk = $_POST['jk'];
+    $kecamatan = $_POST['kecamatan'];
+    $kota = $_POST['kota'];
+    $provinsi = $_POST['provinsi'];
+
+    $query = "INSERT INTO warga (nama, nik, jk, kecamatan, kota, provinsi) VALUES ('$nama', '$nik', '$jk', '$kecamatan', '$kota', '$provinsi')";
+    $koneksi->query($query);
+
+    header("Location: data.php");
+    return 0;
+}
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Tambah warga</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  </head>
-  <body>
+</head>
+
+<body>
     <div class="container">
-    <div class="card shadow" style="width: 35%; margin-left: 35%; margin-top:7%">
+        <div class="card shadow" style="width: 35%; margin-left: 35%; margin-top:7%">
             <form action="" method="post" class="ms-1 mb-1">
-            <h2 style="margin-left: 21%; margin-top:2%">Tambah <b>Warga</b></h2>
+                <h2 style="margin-left: 21%; margin-top:2%">Tambah <b>Warga</b></h2>
                 <div class="card-body">
                     <div class="input-group mb-2">
-                        <input type="text" name="nama" placeholder="Masukkan Nama" style="width: 100%; border-radius:4px" required>
+                        <input type="text" name="nama" placeholder="Masukkan Nama" style="width: 100%; border-radius:4px">
                     </div>
                     <div class="input-group mb-2">
-                        <input type="text" name="nik" placeholder="Masukkan NIK" style="width: 100%; border-radius:4px" required>
+                        <input type="text" name="nik" placeholder="Masukkan NIK" style="width: 100%; border-radius:4px">
                     </div>
                     <div class="input-group mb-2">
                         <select name="jk" id="jk" style="border-radius: 5px;">
                             <option value="">-Jenis Kelamin-</option>
                             <option value="Laki Laki">Laki Laki</option>
                             <option value="Perempuan">Perempuan</option>
-                        </select>    
+                        </select>
                     </div>
                     <div class="input-group mb-2">
-                        <input type="text" name="kecamatan" placeholder="Masukkan Kecamatan" style="width: 100%; border-radius:4px" required>
+                        <input type="text" name="kecamatan" placeholder="Masukkan Kecamatan" style="width: 100%; border-radius:4px">
                     </div>
                     <div class="input-group mb-2">
-                        <input type="text" name="kota" placeholder="Masukkan Kota" style="width: 100%; border-radius:4px" required>
+                        <input type="text" name="kota" placeholder="Masukkan Kota" style="width: 100%; border-radius:4px">
                     </div>
                     <div class="input-group mb-2">
-                        <input type="text" name="provinsi" placeholder="Masukkan Provinsi" style="width: 100%; border-radius:4px" required>
+                        <input type="text" name="provinsi" placeholder="Masukkan Provinsi" style="width: 100%; border-radius:4px">
                     </div>
                     <button class="btn btn-success mt-2" name="submit" type="submit" style="border-radius: 5px; width: 100%">Tambah</button>
                 </div>
@@ -58,7 +73,8 @@
         </div>
     </div>
     <a class="btn btn-primary" style="margin-left: 38%; margin-top:1%" href="data.php">Kembali</a>
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
--- edit.php
+++ edit.php
@@ -1,61 +1,74 @@
 <?php
- include 'config.php';
-  
- $id = $_GET['id'];
+include 'config.php';
 
- $result = $koneksi->query("SELECT * FROM warga WHERE id = $id");
- $row = $result->fetch_assoc();
+$id = $_GET['id'];
 
+$result = $koneksi->query("SELECT * FROM warga WHERE id = $id");
+$row = $result->fetch_assoc();
+
+if (isset($_POST["simpan"])) {
     if (isset($_POST["simpan"])) {
-        $nama = $_POST['nama'];
-        $nik = $_POST['nik'];
-        $jk = $_POST['jk'];
-        $kecamatan = $_POST['kecamatan'];
-        $kota = $_POST['kota'];
-        $provinsi = $_POST['provinsi'];
+        if ($_POST["nama"] == "" | $_POST["nik"] == "" | $_POST["jk"] == "" | $_POST["kecamatan"] == "" | $_POST["kota"] == "" | $_POST["provinsi"] == "") {
+            // cek
+            echo '<script>
+             alert("Jangan ada yang kosong !");
+             windows.location="kelompok-main/edit.php";
+            </script>';
+            return false;
+        }
+    }
 
-        $query = "UPDATE warga SET nama='$nama', nik='$nik', jk='$jk', kecamatan='$kecamatan', kota='$kota', provinsi='$provinsi' WHERE id = $id ";
-        $koneksi->query($query);
+    $nama = $_POST['nama'];
+    $nik = $_POST['nik'];
+    $jk = $_POST['jk'];
+    $kecamatan = $_POST['kecamatan'];
+    $kota = $_POST['kota'];
+    $provinsi = $_POST['provinsi'];
+
+    $query = "UPDATE warga SET nama='$nama', nik='$nik', jk='$jk', kecamatan='$kecamatan', kota='$kota', provinsi='$provinsi' WHERE id = $id ";
+    $koneksi->query($query);
 
     header("Location: data.php");
     return 0;
-    }
+}
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Edit</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  </head>
-  <body>
+</head>
+
+<body>
     <div class="container">
         <div class="card shadow" style="width: 35%;  margin-left: 35%; margin-top:7%">
             <h2 style="margin-left: 31%; margin-top:2%">Edit <b> Warga</b></h2>
             <div class="card-body">
                 <form action="" method="post">
                     <div class="input-group mb-2">
-                        <input type="text" name="nama" placeholder="Masukkan Nama" style="width: 100%; border-radius:4px" value="<?= $row['nama']; ?>" required>
+                        <input type="text" name="nama" placeholder="Masukkan Nama" style="width: 100%; border-radius:4px" value="<?= $row['nama']; ?>">
                     </div>
                     <div class="input-group mb-2">
-                        <input type="text" name="nik" placeholder="Masukkan NIK" style="width: 100%; border-radius:4px" value="<?= $row['nik']; ?>" required>
+                        <input type="text" name="nik" placeholder="Masukkan NIK" style="width: 100%; border-radius:4px" value="<?= $row['nik']; ?>">
                     </div>
                     <div class="input-group mb-2">
                         <select name="jk" id="jk" style="border-radius: 5px;" value="<?= $row['jk']; ?>" required>
                             <option value="">-Jenis Kelamin-</option>
                             <option value="Laki Laki">Laki Laki</option>
                             <option value="Perempuan">Perempuan</option>
-                        </select>    
+                        </select>
                     </div>
                     <div class="input-group mb-2">
-                        <input type="text" name="kecamatan" placeholder="Masukkan Kecamatan" style="width: 100%; border-radius:4px" value="<?= $row['kecamatan']; ?>" required>
+                        <input type="text" name="kecamatan" placeholder="Masukkan Kecamatan" style="width: 100%; border-radius:4px" value="<?= $row['kecamatan']; ?>">
                     </div>
                     <div class="input-group mb-2">
-                        <input type="text" name="kota" placeholder="Masukkan Kota" style="width: 100%; border-radius:4px" value="<?= $row['kota']; ?>" required>
+                        <input type="text" name="kota" placeholder="Masukkan Kota" style="width: 100%; border-radius:4px" value="<?= $row['kota']; ?>">
                     </div>
                     <div class="input-group mb-2">
-                        <input type="text" name="provinsi" placeholder="Masukkan Provinsi" style="width: 100%; border-radius:4px" value="<?= $row['provinsi']; ?>" required>
+                        <input type="text" name="provinsi" placeholder="Masukkan Provinsi" style="width: 100%; border-radius:4px" value="<?= $row['provinsi']; ?>">
                     </div>
                     <button class="btn btn-success" name="simpan" type="submit" style="border-radius: 5px; width: 100%">Simpan</button>
                 </form>
@@ -63,7 +76,8 @@
         </div>
     </div>
     <a class="btn btn-primary" style="margin-left: 38%; margin-top:1%" href="data.php">Kembali</a>
-        
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
